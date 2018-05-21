dc1af6bd83c301189557ee8cb110a8f9f6df4167
Yuchen Liu <steny138@gmail.com>
2018-05-21 13:03:15 +0800

    fix the bug for update model in scrapy piplinemiddleware, that the coding ',' symbol should not be here. if eprice model exist , should update it

--- src/ns_web_crawler/ns_web_crawler/pipelines.py
+++ src/ns_web_crawler/ns_web_crawler/pipelines.py
@@ -42,32 +42,40 @@ class PostgreSqlPipeline(object):
 
     def process_item(self, item, spider):
         if spider.name == "eshop-price-index":
-
             for price in item["prices"]:
-
-                model = GameEPriceModel(
-                    id = uuid4(),
-                    name = item["name"],
-                    country = price["country"]["name"],
-                    eprice=price["price"],
-                    eprice_specified=price["price"],
-                    onsale= price["onsale"],
-                    currency_specified=price["currency"],
-                    name_tw=None,
-                    create_time= datetime.now(),
-                    update_time = item["last_updated"]
-                )
+                model = self.session.query(GameEPriceModel) \
+                                    .filter_by(country = price["country"]["name"], name = item["name"]) \
+                                    .first()
+                if model:
+                    model.eprice=price["price"]
+                    model.eprice_specified=price["price"]
+                    model.currency_specified=price["currency"]
+                    model.onsale= price["onsale"]
+                    model.update_time = item["last_updated"]
+                else:
+                    model = GameEPriceModel(
+                        id = uuid4(),
+                        name = item["name"],
+                        country = price["country"]["name"],
+                        eprice=price["price"],
+                        eprice_specified=price["price"],
+                        onsale= price["onsale"],
+                        currency_specified=price["currency"],
+                        name_tw=None,
+                        create_time= datetime.now(),
+                        update_time = item["last_updated"]
+                    )
                 self.session.add(model)
         elif spider.name == "wiki-country-currency":
-            model = self.session.query(CountryCurrencyModel).filter_by(country = item["country"]).first()
+            model = self.session.query(CountryCurrencyModel).filter_by(country = item["country"],).first()
 
             if model:
-                model.currency = item["currency"],
-                model.country_name=item["country_name"],
-                model.currency_name=item["currency_name"],
-                model.symbol=item["symbol"],
-                model.unit=item["unit"],
-                model.digit= item["digit"],
+                model.currency = item["currency"]
+                model.country_name=item["country_name"]
+                model.currency_name=item["currency_name"]
+                model.symbol=item["symbol"]
+                model.unit=item["unit"]
+                model.digit= item["digit"]
                 model.last_updated = item["last_updated"]
             else:
                 model = CountryCurrencyModel(
