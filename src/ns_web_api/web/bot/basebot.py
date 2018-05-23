7f0117f6366be423fbc606a75f81a0723a09537f
Yuchen Liu <steny138@gmail.com>
2018-05-23 23:53:26 +0800

    analysis the message from user pass, and filter message that the bot will understand it.

--- src/ns_web_api/web/bot/basebot.py
+++ src/ns_web_api/web/bot/basebot.py
@@ -1,9 +1,15 @@
 # -*- coding: utf-8 -*-
 
-
 class BaseBot():
     def send_message(self, text):
         raise NotImplementedError
     
     def reply_message(self, text):
         raise NotImplementedError
+
+class UserProfile():
+    def __init__(self):
+        self.id = ""
+        self.name = ""
+        self.picture = ""
+        self.description = ""
--- src/ns_web_api/web/bot/line.py
+++ src/ns_web_api/web/bot/line.py
@@ -4,13 +4,13 @@ from linebot import (
     LineBotApi, WebhookHandler, WebhookParser
 )
 from linebot.exceptions import (
-    InvalidSignatureError
+    InvalidSignatureError, LineBotApiError
 )
 from linebot.models import (
     MessageEvent, TextMessage, TextSendMessage,
 )
 
-from basebot import BaseBot
+from basebot import BaseBot, UserProfile
 
 # 64xj+m7bdKgcso0DyhkfwhXDTqPoRzhccBi7/HVFEJaXt+lXi8wZc+33QCMQtllW6h9/sLk2d42RGQmgCnIZPpmzzyDopjUH0GUv82Q5gmJZUfNQ6Khh+1S5elc8QWkbrgTHU8OZ8v71xIeIbkxh+QdB04t89/1O/w1cDnyilFU=
 
@@ -25,15 +25,55 @@ class LYCLineBot(BaseBot):
         self.handler = WebhookHandler('80063e1a3a3a1fbd3eb3ce42571cfda1')
         self.parser =  WebhookParser('80063e1a3a3a1fbd3eb3ce42571cfda1')
 
-    def send_message(self, text):
-        self.line_bot_api.push_message('U8c4551a39c652dda7d0f95ca8f3b1b1d', TextSendMessage(text=text))
+    def send_message(self, text, user_id):
+        self.line_bot_api.push_message(user_id, TextSendMessage(text=text))
+
+        return "Send"
     
     def reply_message(self, text, body, signature):
-        events = self.parser.parse(body, signature)
+        try:
+            events = self.parser.parse(body, signature)
 
-        for event in events:
-            reply_text = event.message.text
-            if text :
-                reply_text = text
+            for event in events:
+                self.line_bot_api.reply_message(event.reply_token, self.analysis_message(event.message))
+            
+        except InvalidSignatureError as e:
+            return "authorization failed"
+        except LineBotApiError as e:
+            return e.error.message
+        else:
+            return "OK"
+        
+    def user_profile(self, user_id):
+        line_profile = self.line_bot_api.get_profile(user_id)
+        if not line_profile:
+            return None
 
-            self.line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_text))
+        profile = UserProfile()
+        profile.id = line_profile.user_id
+        profile.name = line_profile.display_name
+        profile.description = line_profile.status_message
+        profile.picture = line_profile.picture_url
+
+        return profile
+
+    def analysis_message(self, message):
+        allow_message_type = { "text":self.reply_by_text, "sticker": self.reply_by_sticker } 
+
+        func = allow_message_type.get(message.type, self.reply_default)
+
+        return func(message)
+
+
+    def reply_by_text(self, message):
+        return  TextSendMessage(text=message.text)
+    
+    def reply_by_sticker(self, message):
+        if int(message.package_id) > 4:
+            return TextSendMessage(text="中文豪難喔，公蝦聽謀捏。")
+        
+        return StickerSendMessage(package_id=message.package_id, sticker_id=message.sticker_id)
+    
+    def reply_default(self, message):
+        return TextSendMessage(text="中文豪難喔，公蝦聽謀捏。")
+
