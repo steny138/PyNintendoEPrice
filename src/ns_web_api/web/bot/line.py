fc0607f09b0a95363d5e22783044cfc72c15a30c
Yuchen Liu <steny138@gmail.com>
2018-05-23 10:50:51 +0800

    now can auto reply message from line bot api

--- src/ns_web_api/web/bot/line.py
+++ src/ns_web_api/web/bot/line.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from linebot import (
-    LineBotApi, WebhookHandler
+    LineBotApi, WebhookHandler, WebhookParser
 )
 from linebot.exceptions import (
     InvalidSignatureError
@@ -23,9 +23,17 @@ class LYCLineBot(BaseBot):
     def __init__(self):
         self.line_bot_api = LineBotApi('64xj+m7bdKgcso0DyhkfwhXDTqPoRzhccBi7/HVFEJaXt+lXi8wZc+33QCMQtllW6h9/sLk2d42RGQmgCnIZPpmzzyDopjUH0GUv82Q5gmJZUfNQ6Khh+1S5elc8QWkbrgTHU8OZ8v71xIeIbkxh+QdB04t89/1O/w1cDnyilFU=')
         self.handler = WebhookHandler('80063e1a3a3a1fbd3eb3ce42571cfda1')
+        self.parser =  WebhookParser('80063e1a3a3a1fbd3eb3ce42571cfda1')
 
     def send_message(self, text):
         self.line_bot_api.push_message('U8c4551a39c652dda7d0f95ca8f3b1b1d', TextSendMessage(text=text))
     
-    def reply_message(self, text):
-        pass
+    def reply_message(self, text, body, signature):
+        events = self.parser.parse(body, signature)
+
+        for event in events:
+            reply_text = event.message.text
+            if text :
+                reply_text = text
+
+            self.line_bot_api.reply_message(event.reply_token, TextSendMessage(text=reply_text))
--- src/ns_web_api/web/linebot_apis.py
+++ src/ns_web_api/web/linebot_apis.py
@@ -1,18 +1,25 @@
 from main import app
 from bot.line import LYCLineBot
 from flask import Blueprint, jsonify, request
-
+import json
 line_bot_api_blueprint = Blueprint('line_bot_api', __name__)
 
 @line_bot_api_blueprint.route('/api/v1/chatbot/line/webhook', methods=['POST'])
 def linechatbot():
+    app.logger.info('line bot webhook had been called.')
+
     # get X-Line-Signature header value
     signature = request.headers['X-Line-Signature']
+    app.logger.info('signature: %s', signature)
 
     # get request body as text
     body = request.get_data(as_text=True)
+    app.logger.info('body: %s', body)
 
-    return jsonify({'signature':signature, 'body': body})
+    bot_service = LYCLineBot()
+    bot_service.reply_message('', body, signature)
+
+    return 'OK'
 
 @line_bot_api_blueprint.route('/api/v1/chatbot/line/message/<msg>')
 def linechatbot_send_message(msg):
