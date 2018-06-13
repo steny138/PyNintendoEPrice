73fa628af3b95e126dd6a8fa3f0661cf1550746b
Yuchen Liu <steny138@gmail.com>
2018-06-13 17:10:33 +0800

    fixed the bug by setting line bot access token will  be passed to the LYCLineBot class in IOC pattern

--- src/ns_web_api/web/linebot_apis.py
+++ src/ns_web_api/web/linebot_apis.py
@@ -16,14 +16,14 @@ def linechatbot():
     body = request.get_data(as_text=True)
     app.logger.info('body: %s', body)
 
-    bot_service = LYCLineBot()
+    bot_service = LYCLineBot(app.config['LINEBOT_CHANNEL_ACCESS_TOKEN'],app.config['LINEBOT_CHANNEL_SECRET'])
     bot_service.reply_message('', body, signature)
 
     return 'OK'
 
 @line_bot_api_blueprint.route('/api/v1/chatbot/line/message/<msg>')
 def linechatbot_send_message(msg):
-    bot_service = LYCLineBot()
+    bot_service = LYCLineBot(app.config['LINEBOT_CHANNEL_ACCESS_TOKEN'],app.config['LINEBOT_CHANNEL_SECRET'])
     bot_service.send_message(msg)
     return jsonify({'message': msg})
 
