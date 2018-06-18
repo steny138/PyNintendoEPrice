aa2fb9d1c9e29c396be56e6c14b81f8cb14f6292
Yuchen Liu <steny138@gmail.com>
2018-06-18 20:26:14 +0800

    add feature  heroku dyno app lycnscrawler with deploy script

--- scripts/heroku_deploy_ns_web_crawler.sh
+++ scripts/heroku_deploy_ns_web_crawler.sh
@@ -3,12 +3,14 @@
 git checkout master
 
 cd ../
-cp -R src/ns_web_crawler/ns_web_crawler/ deploy/deploy.deploy.ns_web_crawler.production
-cp .gitignore deploy/deploy.deploy.ns_web_crawler.production
+cp -R src/ns_web_crawler/ns_web_crawler/ deploy/deploy.ns_web_crawler.production/ns_web_crawler
+rm deploy/deploy.ns_web_crawler.production/ns_web_crawler/.env
+cp .gitignore deploy/deploy.ns_web_crawler.production
 cp -R src/ns_web_crawler/Pipfile deploy/deploy.ns_web_crawler.production/Pipfile
 cp -R src/ns_web_crawler/Pipfile.lock deploy/deploy.ns_web_crawler.production/Pipfile.lock
-cp deploy/procfiles/ns_web_crawler.production deploy/deploy.ns_web_crawler.production/Procfile
-cd deploy/deploy.deploy.ns_web_crawler.production
+cp -R src/ns_web_crawler/scrapy.cfg deploy/deploy.ns_web_crawler.production/scrapy.cfg
+cp deploy/procfiles/ns_web_crawler.production.Procfile deploy/deploy.ns_web_crawler.production/Procfile
+cd deploy/deploy.ns_web_crawler.production
 git add -A
 git commit -m "production deploy for ns_web_crawler"
 git push heroku master -f
