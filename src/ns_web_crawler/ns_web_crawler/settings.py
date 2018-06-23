7921d0571afcd3a3e998fa75037d11ab022b5f64
Yuchen Liu <steny138@gmail.com>
2018-06-23 17:41:20 +0800

    minus the download delay to one second per time with scrapy.

--- src/ns_web_crawler/ns_web_crawler/settings.py
+++ src/ns_web_crawler/ns_web_crawler/settings.py
@@ -36,7 +36,8 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-DOWNLOAD_DELAY = 3
+# The delay unit is second.
+DOWNLOAD_DELAY = 1
 
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
