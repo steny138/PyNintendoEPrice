161e48c095ac9833c697e1a2874dd20dffe0777c
YuchenLiu <steny138@gmail.com>
2019-08-27 23:49:48 +0800

    feat(eshop): remove the unuseage spider & database model

--- src/ns_web_crawler/ns_web_crawler/models/game_model.py
+++ src/ns_web_crawler/ns_web_crawler/models/game_model.py
@@ -8,23 +8,77 @@ Base = declarative_base()
 import datetime
 class GameModel(Base):
     __tablename__ = 'games'
+    """
+    eprice: 基準的遊戲價格
+    country: 基準的遊戲區域
+    currency: 基準的遊戲幣別
 
-    id                  =   Column(UUID(as_uuid=True),
-                                    primary_key=True)
+    onsale_[country]: 該遊戲在該國家是否特價
+    currency_[country]: 該遊戲在該國家的與基準遊戲幣別
+    eprice_[country]: 該遊戲在該國家的售價
+
+    countries : ['AU', 'CA', 'CH', 'IT', 'MX', 'NL', 'NZ', 'US', 'ZA']
+    """
+    id                  =   Column(UUID(as_uuid=True),primary_key=True)
     name                =   Column(String(100),nullable=True)
     name_tw             =   Column(String(100),nullable=True)
     name_en             =   Column(String(100),nullable=True)
     name_jp             =   Column(String(100),nullable=True)
-    hotsale             =   Column(Boolean,default=False)
-    onsale              =   Column(Boolean,default=False)
+
+    eprice              =   Column(Float,nullable=False)
+    country             =   Column(String(10),nullable=False)
+    currency            =   Column(String(10),nullable=False)
+
+    onsale_jp           =   Column(Boolean, default=False)
+    currency_jp         =   Column(String(10), nullable=False)
+    eprice_jp           =   Column(Float, nullable=False)
+
+    onsale_us           =   Column(Boolean,default=False)
+    currency_us         =   Column(String(10),nullable=False)
+    eprice_us           =   Column(Float,nullable=False)
+
+    onsale_ca           =   Column(Boolean,default=False)
+    currency_ca         =   Column(String(10),nullable=False)
+    eprice_ca           =   Column(Float,nullable=False)
+
+    onsale_za           =   Column(Boolean,default=False)
+    currency_za         =   Column(String(10),nullable=False)
+    eprice_za           =   Column(Float,nullable=False)
+
+    onsale_nz           =   Column(Boolean,default=False)
+    currency_nz         =   Column(String(10),nullable=False)
+    eprice_nz           =   Column(Float,nullable=False)
+
+    onsale_nl           =   Column(Boolean,default=False)
+    currency_nl         =   Column(String(10),nullable=False)
+    eprice_nl           =   Column(Float,nullable=False)
+
+    onsale_mx           =   Column(Boolean,default=False)
+    currency_mx         =   Column(String(10),nullable=False)
+    eprice_mx           =   Column(Float,nullable=False)
+
+    onsale_it           =   Column(Boolean,default=False)
+    currency_it         =   Column(String(10),nullable=False)
+    eprice_it           =   Column(Float,nullable=False)
+
+    onsale_ch           =   Column(Boolean,default=False)
+    currency_ch         =   Column(String(10),nullable=False)
+    eprice_ch           =   Column(Float,nullable=False)
+
+    onsale_au           =   Column(Boolean,default=False)
+    currency_au         =   Column(String(10),nullable=False)
+    eprice_au           =   Column(Float,nullable=False)
+
     create_time         =   Column(String(500), nullable=False)
     update_time         =   Column(DateTime,nullable=False)
 
-    def __init__(self,id,name,name_tw=None,hotsale=None,create_time=None,update_time=None):
+    def __init__(self,id,name,country,eprice,currency=None,name_tw=None,create_time=None,update_time=None):
         self.id                     =   id
         self.name                   =   name
         self.name_tw                =   name_tw
-        self.hotsale                =   hotsale
+        self.country                =   country
+        self.eprice                 =   eprice
+        self.currency               =   currency
         self.create_time            =   create_time
         self.update_time            =   update_time
 
--- src/ns_web_crawler/ns_web_crawler/pipelines.py
+++ src/ns_web_crawler/ns_web_crawler/pipelines.py
@@ -9,8 +9,7 @@ import logging
 from uuid import uuid4
 from datetime import datetime
 from ns_web_crawler.connections import postgresql_conn
-from ns_web_crawler.models.game_eprice_model import GameEPriceModel
-from ns_web_crawler.models.country_mapping_currency_model import CountryCurrencyModel
+from ns_web_crawler.models.game_model import GameModel
 
 class NsWebCrawlerPipeline(object):
     def process_item(self, item, spider):
@@ -43,7 +42,7 @@ class PostgreSqlPipeline(object):
     def process_item(self, item, spider):
         if spider.name == "eshop-price-index":
             for price in item["prices"]:
-                model = self.session.query(GameEPriceModel) \
+                model = self.session.query(GameModel) \
                                     .filter_by(country = price["country"]["name"], name = item["name"]) \
                                     .first()
                 if model:
@@ -52,7 +51,7 @@ class PostgreSqlPipeline(object):
                     model.currency_specified=price["currency"]
                     model.update_time = item["last_updated"]
                 else:
-                    model = GameEPriceModel(
+                    model = GameModel(
                         id = uuid4(),
                         name = item["name"],
                         country = price["country"]["name"],
@@ -65,53 +64,17 @@ class PostgreSqlPipeline(object):
                         update_time = item["last_updated"]
                     )
                 self.session.add(model)
-        elif spider.name == "wiki-country-currency":
-            model = self.session.query(CountryCurrencyModel).filter_by(country = item["country"],).first()
-
-            if model:
-                model.currency = item["currency"]
-                model.country_name=item["country_name"]
-                model.currency_name=item["currency_name"]
-                model.symbol=item["symbol"]
-                model.unit=item["unit"]
-                model.digit= item["digit"]
-                model.last_updated = item["last_updated"]
-            else:
-                model = CountryCurrencyModel(
-                    id = uuid4(),
-                    country = item["country"],
-                    currency = item["currency"],
-                    country_name=item["country_name"],
-                    currency_name=item["currency_name"],
-                    symbol=item["symbol"],
-                    unit=item["unit"],
-                    digit= item["digit"],
-                    last_updated = item["last_updated"]
-                )
-
-            self.session.add(model) 
         elif spider.name == "gamer-ns-games":
-
             if item["name_english"]:
                 logging.info("update game name: %s", item["name_english"].strip())
                 filter_name = item["name_english"].strip()
                 self.session \
-                    .query(GameEPriceModel) \
+                    .query(GameModel) \
                     .filter_by(name = filter_name) \
                     .update(dict(name_tw=item["name_chinese"],
-                                name_en=item["name_english"],
-                                name_jp=item["name_japaness"]))
-        elif spider.name == "eshop-price-onsale":
-            model = self.session.query(GameEPriceModel) \
-                                .filter_by(country = item["country"], name = item["name"], currency_specified = item["currency"]) \
-                                .first()
-            if model:
-                model.eprice = item["price"]
-                model.onsale = item["onsale"]
+                                 name_en=item["name_english"],
+                                 name_jp=item["name_japaness"]))
 
-            if item["onsale"]:
-                logging.info("the game %s in %s is onsale or not: %s", item["name"], item["country"], item["onsale"])
-            
         self.session.commit()
 
         return item
