b8f2daaa32c3e7f4f2ede53397a6561f7d7c6f06
YuchenLiu <steny138@gmail.com>
2019-09-01 13:45:02 +0800

    feat(eshop): crawl the nintendo europe eshop games with eshop_eu.py

---
+++ src/ns_web_crawler/ns_web_crawler/eshop/eshop_eu.py
@@ -0,0 +1,54 @@
+import json
+import requests
+
+import copy
+# 逗點隔開
+# https://api.ec.nintendo.com/v1/price?country=GB&lang=en&ids=70010000006852
+# %2C70010000019183%2C70010000013914%2C70010000019160%2C70010000019341
+
+import os
+EU_GET_GAMES_URL   = os.getenv('EU_GET_GAMES_URL', 'http://search.nintendo-europe.com/{locale}/select')
+EU_GAME_CHECK_CODE = os.getenv('EU_GAME_CHECK_CODE', '70010000000184')
+
+class EShopEUApi(object):
+    EU_GET_GAMES_OPTIONS = {
+        'fq': 'type:GAME AND system_type:nintendoswitch* AND product_code_txt:*',
+        'q': '*',
+        'sort': 'sorting_title asc',
+        'start': '0',
+        'wt': 'json',
+    }
+
+    EU_DEFAULT_LOCALE = 'en'
+    EU_GAME_LIST_LIMIT = 9999
+
+    def __init__(self, *args):
+        pass
+
+    def get_all_games(self):
+        """Get all games in Europe.
+        """
+        all_games = {}
+        response = self.__get_api_result()
+        game_count = response['response']['numFound']
+        print(f"found {game_count} games in europe.")
+        for game in response['response']['docs']:
+            if not game['fs_id'] in all_games :
+                all_games[game['fs_id']] = game
+
+        print(len(all_games))
+    
+    def __get_api_result(self):
+        querystring = copy.deepcopy(self.EU_GET_GAMES_OPTIONS)
+        querystring['rows'] = self.EU_GAME_LIST_LIMIT
+
+        url = EU_GET_GAMES_URL.replace('{locale}', self.EU_DEFAULT_LOCALE)
+
+        r = requests.get(url, params=querystring)
+        print(r.url)
+        return r.json()
+
+
+if __name__ == '__main__':
+    req = EShopEUApi()
+    req.get_all_games()
