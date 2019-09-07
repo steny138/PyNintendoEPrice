628465d498bf6637c2c2edc63e817c90bb887834
YuchenLiu <steny138@gmail.com>
2019-09-07 13:02:58 +0800

    feat(eshop): modified the scrapy item structure.

--- src/ns_web_crawler/ns_web_crawler/items/eshop_price.py
+++ src/ns_web_crawler/ns_web_crawler/items/eshop_price.py
@@ -3,12 +3,8 @@
 import scrapy
 
 class EshopProductItem(scrapy.Item):
-    name = scrapy.Field()
-    prices = scrapy.Field()
+    nsuid        = scrapy.Field()
+    name         = scrapy.Field()
+    category     = scrapy.Field()
+    prices       = scrapy.Field()
     last_updated = scrapy.Field(serializer=str)
-
-class EshopPriceCountryItem(scrapy.Item):
-    country = scrapy.Field()
-    currency = scrapy.Field()
-    price = scrapy.Field()
-    onsale = scrapy.Field()
