fb9cb6a06f05a5b1456e5297ab468a956ef32d6f
YuchenLiu <steny138@gmail.com>
2019-09-08 18:56:10 +0800

    feat(logging): setting default logging config

---
+++ src/ns_web_crawler/ns_web_crawler/__init__.py
@@ -0,0 +1,8 @@
+import sys
+import logging
+
+logging.basicConfig(
+    level=logging.INFO,
+    format="[%(asctime)s] %(levelname)s [%(name)s.%(funcName)s:%(lineno)d] %(message)s",
+    datefmt="%H:%M:%S",
+    stream=sys.stdout)
