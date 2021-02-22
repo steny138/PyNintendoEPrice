5025bd4a2ddce043e7173b9135003409d633bc9f
Steny Liu <steny.liu@epictech.com.tw>
2021-02-22 09:22:31 +0800

    fix: remove empty string in match message

--- src/ns_web_api/web/events/analyzer.py
+++ src/ns_web_api/web/events/analyzer.py
@@ -4,11 +4,13 @@ from .lifes import LifeEvent
 from .traffic import TrafficEvent
 from .diving import DivingEvent
 
+
 class EventAyalyzer(object):
     _instance = None
+
     def __new__(cls, *args, **kw):
         if not cls._instance:
-            cls._instance = super(EventAyalyzer, cls).__new__(cls, *args, **kw)  
+            cls._instance = super(EventAyalyzer, cls).__new__(cls, *args, **kw)
             cls._instance.events = []
 
             cls._instance.events.append(GamesEvent())
@@ -16,10 +18,13 @@ class EventAyalyzer(object):
             cls._instance.events.append(TrafficEvent())
             cls._instance.events.append(DivingEvent())
 
-        return cls._instance 
+        return cls._instance
 
     def match(self, fragments):
-        match_events = [(lambda x: x.occurs(fragments))(x) for x in self.events]
-        return match_events
+        match_events = [(lambda x: x.occurs(fragments))(x)
+                        for x in self.events]
+
+        return list(filter(None, match_events))  # remove empty string
+
 
 analyzer = EventAyalyzer()
