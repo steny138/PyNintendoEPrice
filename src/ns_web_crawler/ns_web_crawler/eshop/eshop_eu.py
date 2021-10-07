d5594a76e9dde55941c9cbf7ac3fcdbd6f5e12e0
Yuchen Liu <steny138@gmail.com>
2021-10-07 23:16:06 +0800

    fix(crawler): fix list cannot iterator

--- src/ns_web_crawler/ns_web_crawler/eshop/eshop_eu.py
+++ src/ns_web_crawler/ns_web_crawler/eshop/eshop_eu.py
@@ -49,7 +49,7 @@ class EShopEUApi(object):
             if 'nsuid_txt' not in game:
                 continue
 
-            gameid = next(game.get('nsuid_txt'))
+            gameid = next(iter(game.get('nsuid_txt')))
             gamecode = ''.join(game.get('product_code_txt', ['unrelease']))
             if check_nsuid(gameid) and gameid not in all_games:
 
